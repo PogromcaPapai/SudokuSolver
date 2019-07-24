864194b5548604c3373eafab3fae832498063588
Jakub Dakowski <jakubdakowski@gmail.com>
2019-07-24 13:55:28 +0200

    Created proper docstrings

--- main.py
+++ main.py
@@ -5,6 +5,13 @@ import const
 import solve
 
 def printrow(row, table):
+    """
+    Prints a series of nine elements from a list
+
+    Arguments:
+        row : int : which series of element to print
+        table : list : list of elements to print
+    """
     text = ''
     for i in range(9*row,9*row+9):
         digit = str(table[i])
@@ -13,15 +20,23 @@ def printrow(row, table):
     print(text)
 
 def printwhole(table):
+    """
+    Prints whole list in form of a 9x9 array
+
+    Arguments:
+        table : list : list of elements to print
+    """
     for i in range(9):
         printrow(i, table)
     print('----')
 
 def end_check(table):
+    """
+    Checks if all cells in the table are properly solved
+    """
     return reduce(lambda x, y: x+y.get_value(), table, 0)==405
 
 if __name__ == "__main__":
-    const.createfield()
     table = const.construct()
     cases = solve.env(table)
     start = perf_counter() #Time measurement starts
